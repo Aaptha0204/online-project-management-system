a24f037cd7afb3fadba2d20623230eca21a5d9de
Anagha Srinivasa <60839370+anaghasrinivas@users.noreply.github.com>
2022-04-17 11:59:08 +0530

    Update LoginDAO.java

--- src/dao/LoginDAO.java
+++ src/dao/LoginDAO.java
@@ -3,7 +3,6 @@ package dao;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
-
 import dbutil.DBUtil;
 import pojo.LoginInfo;
 
@@ -11,9 +10,10 @@ public class LoginDAO {
 
 	public static boolean isUserValid(LoginInfo userDetails)
 	{
+		
 		boolean validStatus = false;
-		try
-		{
+		
+		try{
 			Connection conn = DBUtil.getConnection();
 			Statement st= conn.createStatement();
 			ResultSet rs= st.executeQuery("SELECT * FROM login_info WHERE userName='"+userDetails.getUserName()+"' AND password='"+userDetails.getPassword()+"'");
@@ -25,10 +25,10 @@ public class LoginDAO {
 			DBUtil.closeConnection(conn);
 			
 		}
-		catch(Exception e)
-		{
+		catch(Exception e){
 			e.printStackTrace();
 		}
 		return validStatus;
+		
 	}
 }
